82f5cbcdd095d5cb2c43e6673a2d3e883d7c0089
ManojTGN <manoj.thunderviz@gmail.com>
2022-08-13 09:18:33 +0530

    Added Main Screen

--- src/ParticleSystem/Game.py
+++ src/ParticleSystem/Game.py
@@ -5,8 +5,8 @@ from ParticleSystem.Screen import Screen
 class Game:
     def __init__(self) -> None:
         self.isRunning = False
-        self.Screen = Screen()
         self.WINDOW = pygame.display.set_mode()
+        self.Screen = Screen()
 
     def eventHandler(self) -> None:
         for event in pygame.event.get():
@@ -16,8 +16,8 @@ class Game:
 
     def run(self) -> None:
         if(self.isRunning): return
-
         self.isRunning = True
+        
         while(self.isRunning):
             self.WINDOW.fill(COLOR_BLACK)
             self.Screen.render()
--- src/ParticleSystem/Screen.py
+++ src/ParticleSystem/Screen.py
@@ -1,6 +1,26 @@
+import pygame
+from ParticleSystem.Configs import *
+
 class Screen:
     def __init__(self) -> None:
-        pass
-    
+        self.WINDOW = pygame.display.get_surface()
+
+        self.WINDOW_SIZE = self.WINDOW.get_size()
+        self.RENDERER_WINDOW = pygame.Surface(((self.WINDOW_SIZE[0]/10)*7,self.WINDOW_SIZE[1]))
+        self.SETTING_WINDOW = pygame.Surface(((self.WINDOW_SIZE[0]/10)*3,self.WINDOW_SIZE[1]))
+
+    def render_rendererWindow(self) -> None:
+        pygame.draw.rect(self.RENDERER_WINDOW,COLOR_WHITE,pygame.Rect(0,0,self.RENDERER_WINDOW.get_size()[0],self.RENDERER_WINDOW.get_size()[1]),4)
+
+    def render_settingsWindow(self) -> None:
+        pygame.draw.rect(self.SETTING_WINDOW,COLOR_WHITE,pygame.Rect(0,0,self.SETTING_WINDOW.get_size()[0],self.SETTING_WINDOW.get_size()[1]),4)
+        
+
+    def BlitToMainScreen(self) -> None:
+        self.WINDOW.blit(self.RENDERER_WINDOW,(0,0))
+        self.WINDOW.blit(self.SETTING_WINDOW,(self.RENDERER_WINDOW.get_size()[0],0))
+
     def render(self) -> None:
-        pass
+        self.render_rendererWindow()
+        self.render_settingsWindow()
+        self.BlitToMainScreen()
